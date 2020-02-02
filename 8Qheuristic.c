121a0dc8cb91568a76d6efb52ed4fc90ca7edaea
Hjain518 <49115022+Hjain518@users.noreply.github.com>
2020-02-03 00:48:01 +0530

    Add files via upload

---
+++ 8Qheuristic.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+int falseCount(int arr[8][8],int row,int column){
+    int cnt=0;
+    //vartical
+    for(int i=0;i<8;i++){
+        if(arr[i][column]==1){
+            cnt++;
+        }
+    }
+    //digonal1
+    for(int i=row-1,j=column-1;i>=0&&j>=0;i--,j--){
+        if(arr[i][j]==1){
+            cnt++;
+        }
+    }
+    for(int i=row+1,j=column+1;i<=7&&j<=7;i++,j++){
+        if(arr[i][j]==1){
+            cnt++;
+        }
+    }
+    //diagonal2
+    for(int i=row-1,j=column+1;i>=0&&j<=7;i--,j++){
+        if(arr[i][j]==1){
+            cnt++;
+        }
+    }
+    for(int i=row+1,j=column-1;i<=7&&j>=0;i++,j--){
+        if(arr[i][j]==1){
+            cnt++;
+        }
+    }
+    return cnt;
+}
+void print(int arr[8][8]){
+    for(int i=0;i<8;i++){
+        for(int j=0;j<8;j++){
+            printf("%d  ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+void Queen8(int arr[8][8]){
+    int row=0,cnt=8,column=0,cnt1;
+    for(int i=0;i<8;i++){
+        cnt=8;
+        for(int j=0;j<8;j++){
+            arr[i][j]=0;
+            cnt1=falseCount(arr,i,j);
+            if(cnt>cnt1){
+                cnt=cnt1;
+                row=i;
+                column=j;
+            }
+            //printf<<cnt1<<"  ";
+        }
+        //printf<<"\n";
+        arr[row][column]=1;
+        row=i+1;
+        column=0;
+    }
+    print(arr);
+}
+
+int main(){
+    int arr[8][8],row,cnt;
+    for(int i=0;i<8;i++){
+        for(int j=1;j<8;j++){
+            arr[i][j]=0;
+        }
+    }
+    for(int k=0;k<100;k++)
+{
+    for(int u=0;u<100;u++)
+    {
+    }
+}
+    for(int i=0;i<8;i++){
+        arr[i][0]=1;
+    }
+    Queen8(arr);
+}
